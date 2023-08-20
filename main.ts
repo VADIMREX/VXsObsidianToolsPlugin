a34a9a08f380b8c53f39e790d0f0a28ac88b91f9
Вадим <nerzhularthas@gmail.com>
2023-08-21 00:35:16 +0600

    Getting template core plugin

--- main.ts
+++ main.ts
@@ -1,81 +1,116 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface VXsToolsPluginSettings {
-	mySetting: string;
+	useCoreTemplateFolder: boolean;
+	templateFolder: string;
 }
 
 const DEFAULT_SETTINGS: VXsToolsPluginSettings = {
-	mySetting: 'default'
+	useCoreTemplateFolder: true,
+	templateFolder: 'default'
+}
+
+interface CoreTemplatePlugin {
+	options: { folder: string };
+	templateFiles: TFile[]
 }
 
 export default class VXsToolsPlugin extends Plugin {
 	settings: VXsToolsPluginSettings;
 
+	get coreTemplatePlugin(): CoreTemplatePlugin | null {
+		let coreTemplatePlugin: any = null;
+		try {
+			let app: any = this.app
+			for (let tab of app.setting.pluginTabs) {
+				if (tab.id !== "templates") continue;
+				coreTemplatePlugin = tab.instance;
+				break;
+			}
+		}
+		catch (e) {
+
+		}
+		return coreTemplatePlugin;
+	}
+
+	get coreTemplateFolder() {
+		let coreTemplatePlugin = this.coreTemplatePlugin;
+		if (null === coreTemplatePlugin) return "";
+		if (null === coreTemplatePlugin.options) return "";
+		if (null === coreTemplatePlugin.options.folder) return "";
+		return coreTemplatePlugin.options.folder;
+	}
+
+	get templateFolder() {
+		if (this.settings.useCoreTemplateFolder) return this.coreTemplateFolder;
+		else return this.settings.templateFolder;
+	}
+
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		if (this.settings.useCoreTemplateFolder && null === this.coreTemplatePlugin) this.settings.useCoreTemplateFolder = false;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		const vxsInsertTemplate = () => {
+			var plugin = this.coreTemplatePlugin;
 
-		// This adds a simple command that can be triggered anywhere
+			var templateFolderPath = this.templateFolder;
+			if (!String.isString(templateFolderPath))  {
+				//plugin.
+				//(new fI(tf.plugins.templates.msgNoFolderSet()),[2])
+				return;
+			}
+
+			var templateFolder = this.app.vault.getAbstractFileByPath(templateFolderPath);
+
+			//let r = 
+			//this.app.vault.getAbstractFileByPath(r);
+			//if ()
+
+			//new SampleModal(this.app).open();
+			//this.app.workspace.getActiveViewOfType(MarkdownView);
+
+			/*
+			var e, t, n, templateFolder, r, o, a, s = this;
+
+			t = (e = this).app,
+				n = e.options,
+				(templateFolder = n.folder) && String.isString(templateFolder) ? (r = he(Od(templateFolder).normalize("NFC")),
+					(o = t.vault.getAbstractFileByPath(r)) && o instanceof QE ? (this.templateFiles = [],
+						(a = function (e) {
+							for (var t = 0, n = e.children; t < n.length; t++) {
+								var i = n[t];
+								i instanceof $E && "md" === i.extension ? s.templateFiles.push(i) : i instanceof QE && a(i)
+							}
+						}
+						)(o),
+						new VQ(t, this).open(),
+						[2]) : (new fI(tf.plugins.templates.msgFailFolderNotFound({
+							folderOption: templateFolder
+						})),
+							[2])) 
+													: (new fI(tf.plugins.templates.msgNoFolderSet()),[2])
+			*/
+
+			
+
+		}
+
+		const ribbonIconVXsTemplate = this.addRibbonIcon('lucide-files', "VX's insert template", evt => vxsInsertTemplate());
+		ribbonIconVXsTemplate.addClass('vxs-plugin-ribbon-class');
+
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'vxs-tools-plugin-insert-template',
+			name: "VX's Insert template",
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				vxsInsertTemplate();
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new VXsToolsPluginSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -97,12 +132,12 @@ class SampleModal extends Modal {
 	}
 
 	onOpen() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.setText('Woah!');
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
@@ -116,19 +151,36 @@ class VXsToolsPluginSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		let [useCoreTemplateSetting, templateFolderSetting] = [
+			new Setting(containerEl)
+				.setName('Use core template settings')
+				.setDesc('')
+				.addToggle(toggle => toggle
+					.setDisabled(null == this.plugin.coreTemplatePlugin)
+					.setValue(this.plugin.settings.useCoreTemplateFolder)
+					.onChange(async (value) => {
+						this.plugin.settings.useCoreTemplateFolder = value;
+						if (value && this.plugin.coreTemplatePlugin) {
+							this.plugin.settings.templateFolder = this.plugin.coreTemplateFolder;
+						}
+						await this.plugin.saveSettings();
+					})),
+
+			new Setting(containerEl)
+				.setName('Template folder location')
+				.setDesc('Files in this folder will be available as templates.')
+				.addText(text => text
+					.setDisabled(this.plugin.settings.useCoreTemplateFolder)
+					.setPlaceholder('')
+					.setValue(this.plugin.settings.templateFolder)
+					.onChange(async (value) => {
+						this.plugin.settings.templateFolder = value;
+						await this.plugin.saveSettings();
+					}))
+		];
 	}
 }
